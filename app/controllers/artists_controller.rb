42246a3942cf1e53d864e34364af6c7a9d294533
MMcClure11 <mcclme02@luther.edu>
2020-07-08 13:58:49 -0500

    adds before_action to artists controller

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,15 +1,15 @@
 class ArtistsController < ApplicationController
+  before_action :get_artist, only: [:show, :edit, :update, :delete]
+  before_action :new_artist, only: [:new]
 
   def index
     @artists = Artist.all
   end
   
   def show
-    @artist = Artist.find(params[:id])
   end
 
   def new
-    @artist = Artist.new
   end
 
   def create
@@ -19,16 +19,22 @@ class ArtistsController < ApplicationController
   end
 
   def edit
-    @artist = Artist.find(params[:id])
   end
 
   def update
-    @artist = Artist.find(params[:id])
     @artist.update(artist_params)
     redirect_to artist_path(@artist)
   end
 
   private
+
+  def get_artist
+    @artist = Artist.find(params[:id])
+  end
+
+  def new_artist
+    @artist = Artist.new
+  end
 
   def artist_params
     params.require(:artist).permit!
