a4fcb073861042ca17c44304ae2c16d5eef20f2c
dylan-williamson <dylwilliamson710@gmail.com>
2020-08-04 14:16:52 -0400

    --- Auto-Git Commit ---

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,15 +1,36 @@
 class GenresController < ApplicationController
 
-    def edit
+    def index
+        @genres = Genre.all
+    end
 
+    def show 
+        @genre = genre.find(params[:id])
     end
 
     def new
-
+        @genre = genre.new
     end
 
-    def show
-
+    def create
+        @genre = genre.new(genre_params)
+        @genre.save
+        redirect_to genre_path(@genre)
     end
 
+    def edit
+        @genre = genre.find(params[:id])
+    end
+
+    def update
+        @genre = genre.find(params[:id])
+        @genre.update(genre_params)
+        redirect_to genre_path(@genre)
+    end
+
+    private
+
+        def genre_params
+            params.require(:genre).permit(:name)
+        end
 end
